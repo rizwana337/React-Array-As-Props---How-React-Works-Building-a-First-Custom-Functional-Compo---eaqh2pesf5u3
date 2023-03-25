0ac1425024ed06a444438adfdbfb757bebedea46
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-03-25 18:09:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,21 @@ import React from 'react'
 import '../styles/App.css';
 const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
 const Join = (props) =>{
+  const { words } = props;
+  const joinedWords = words.join(',');
   return(
     <div id ="join">
+    {joinedWords}
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
     </div>
   )
 }
 const App = () => {
-
+const arr = ['hello', 'world', 'React'];
   return (
     <div id="main">
-      <Join  />
+    
+      <Join words={arr} />
     </div>
   )
 }
