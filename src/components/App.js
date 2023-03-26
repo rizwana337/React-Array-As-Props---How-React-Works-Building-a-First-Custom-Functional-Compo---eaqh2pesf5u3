6ef015f595b22ae417f325df08f2fbfd6ec2d80c
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-03-26 13:59:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,15 @@ import React from 'react'
 import '../styles/App.css';
 const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
 const Join = (props) =>{
-  const { words } = props;
-  const joinedWords = words.join(',');
+    const { words } = props;
+  const commaSeparated = words.join(',');
   return(
     <div id ="join">
-      {joinedWords.replace(/hello|world|React/g, match => match[0])}
+      {commaSeparated}
     </div>
   )
 }
 const App = () => {
-const arr = ['hello', 'world', 'React'];
   return (
     <div id="main">
     
